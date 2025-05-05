b0f7ece8510c7c6b0963201a0d176e4bd7287195
Hakho05 <daubykyllian04@gmail.com>
2025-05-05 10:41:51 +0200

    c

--- src/App.java
+++ src/App.java
@@ -7,18 +7,7 @@ public class App {
         System.out.println("Choisissez 1 ou 2");
     }
 
-    public static void afficherMenuDemandeur(){
-        System.out.println("Quel type de véhicule que vous souhaitez :");
-        System.out.println("1 : Longue distance");
-        System.out.println("2 : Courte distance");
-        System.out.println("3 : Electrique");
-        System.out.println("4 : Thermique");
-        System.out.println("5 : Hybrid");
-
-    }
-    public static void choixDemand(int choixDemandeur){
-        System.out.println("");
-    }
+    
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------
 */
@@ -31,9 +20,9 @@ public class App {
         
         if(choix == 1){
             System.out.println("Vous êtes demandeur");
-            afficherMenuDemandeur();
+            Demandeur.afficherMenuDemandeur();
             int choixDemandeur = sc.nextInt();
-            
+
         }else if(choix == 2){
             System.out.println("Vous êtes administrateur");
         }else{
