0e27da92b4220532e47ff8b723ee1609cc742d51
iony59100 <iionnyyyy@gmail.com>
2025-05-05 10:41:25 +0200

    passerelle

--- src/Administrateur.java
+++ src/Administrateur.java
@@ -1,3 +1,16 @@
+import java.sql.*;
+import java.util.ArrayList;
+import Passerelle.Passerelle;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Administrateur {
-    
+    private static Connection conn=Passerelle.connexionBDD();
+
+    public static void afficherDemandeLocation() {
+        
+    }
+
+
+
 }
